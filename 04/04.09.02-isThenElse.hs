d49aa7d84c25b5c81db86d9f85c95968f155cbdd
Jakub <nawias@gmail.com>
2017-03-08 21:51:59 +0100

    adds 10 exercises from chapter 4.09

---
+++ 04/04.09.02-isThenElse.hs
@@ -0,0 +1,5 @@
+myAbs :: Integer -> Integer
+myAbs x =
+  if (x >= 0)
+    then x
+    else x * (-1)
---
+++ 04/04.09.03-function.hs
@@ -0,0 +1,2 @@
+f::(a,b)->(c,d)->((b,d),(a,c))
+f a b = (,) ((,) (snd a) (snd b)) ((,) (fst a) (fst b))
